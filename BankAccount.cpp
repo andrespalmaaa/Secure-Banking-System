0919cd19a961b6cfa4049a3859d539aef6924ae4
andrespalmaaa <andresrobertopalma@hotmail.com>
2026-01-07 23:02:47 -0500

    Implement BankAccount class with transaction methods

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,1 +1,45 @@
+#include "BankAccount.hpp"
+#include <string>
+#include <iostream>
 
+using namespace std;
+
+//conctsructor in cpp
+BankAccount::BankAccount(std::string accNumber, double initialBalance): accountNumber(accNumber), balance(initialBalance) {
+}
+
+std::string BankAccount::getAccountNumber() const {
+  return accountNumber;
+}
+
+double BankAccount::getBalance() const {
+  return balance;
+}
+
+//Update Account Number
+void BankAccount::setAccountNumber(std::string newAccountNumber) {
+  accountNumber = newAccountNumber;
+}
+
+//Update Balance
+void BankAccount::setBalance(double newBalance) {
+  balance = newBalance;
+}
+
+//Deposit Method 
+void BankAccount::Transaction::deposit(BankAccount& account, double amount) {
+  account.balance += amount;
+  std::cout << "Deposited: " << amount
+            << " | New Balance: " << account.balance << std::endl;
+}
+
+//Withdrawal Method
+void BankAccount::Transaction::withdraw(BankAccount& account, double amount) {
+  if (account.balance >= amount) {
+    account.balance -= amount;
+    std::cout << "Withdrew: " << amount
+              << " | New Balance: " << account.balance << std::endl;
+  } else {
+    std::cout << "Withdrawal failed. Insufficient funds." << std::endl;
+  }
+}
