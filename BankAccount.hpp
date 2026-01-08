a1c3269417e9265420a923c1e46e7271a83a19b3
andrespalmaaa <andresrobertopalma@hotmail.com>
2026-01-07 23:02:29 -0500

    Define BankAccount class with transaction methods

--- BankAccount.hpp
+++ BankAccount.hpp
@@ -1,1 +1,29 @@
+#ifndef BANKACCOUNT_HPP
+#define BANKACCOUNT_HPP
 
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class BankAccount {
+private:
+  string accountNumber;
+  double balance;
+
+public:
+  BankAccount(string accNumber, double initialBalance);
+
+  std::string getAccountNumber() const;
+  double getBalance() const;
+
+  void setAccountNumber(std::string newAccountNumber);
+  void setBalance(double newBalance);
+
+  class Transaction {
+  public: 
+    void deposit(BankAccount& account, double amount);
+    void withdraw(BankAccount& account, double amount);
+  };
+};
+#endif
